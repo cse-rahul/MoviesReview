fe8c07c3f53185127ac98e986864f9d5d455ac28
Rahul <rahulramanmaurya@gmail.com>
2024-07-30 19:40:11 +0530

    commit changes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,13 +8,12 @@ import { getFirestore, collection } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBPKag7p0qR_3jurHLYK7urI8QyLfubIoI",
-  authDomain: "moviesreview-project.firebaseapp.com",
-  projectId: "moviesreview-project",
-  storageBucket: "moviesreview-project.appspot.com",
-  messagingSenderId: "881276370494",
-  appId: "1:881276370494:web:f8136f66233d0448fa5b6f",
-  measurementId: "G-Z137K67RPN",
+  apiKey: "AIzaSyB8WXJr-RspM56Wf9DqDZUR8CpycNHUGRA",
+  authDomain: "moviesreview-1fb29.firebaseapp.com",
+  projectId: "moviesreview-1fb29",
+  storageBucket: "moviesreview-1fb29.appspot.com",
+  messagingSenderId: "277286676566",
+  appId: "1:277286676566:web:14e83c776671f4f719f3f7",
 };
 
 // Initialize Firebase
