50b8ab49bbb88a2af0301541ea7ffa19d3cbe089
Rahul Maurya <rahulramanmaurya@gmail.com>
2024-09-19 16:53:38 +0530

    new commit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,25 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection } from "firebase/firestore";
+import {getFirestore, collection} from 'firebase/firestore'
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB8WXJr-RspM56Wf9DqDZUR8CpycNHUGRA",
-  authDomain: "moviesreview-1fb29.firebaseapp.com",
-  projectId: "moviesreview-1fb29",
-  storageBucket: "moviesreview-1fb29.appspot.com",
-  messagingSenderId: "277286676566",
-  appId: "1:277286676566:web:14e83c776671f4f719f3f7",
+  apiKey: "AIzaSyD0Pz0c3XMuZm8t7R4jKkriJn4zaIzc6WU",
+  authDomain: "moviesreviewapp-99c47.firebaseapp.com",
+  projectId: "moviesreviewapp-99c47",
+  storageBucket: "moviesreviewapp-99c47.appspot.com",
+  messagingSenderId: "600387637488",
+  appId: "1:600387637488:web:4f90efc6d823e695447235",
+  measurementId: "G-QQN26YCLSY"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
-
+const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const moviesRef = collection(db, "movies");
 export const reviewsRef = collection(db, "reviews");
